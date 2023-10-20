55f1c270948073f2e4b84a2394521b840e7f36c8
Brooks-Alexander <brooks.alexander.r@gmail.com>
2023-10-20 23:05:17 +0200

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/alexanders-react-portfolio">
+    <BrowserRouter basename="/Portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
